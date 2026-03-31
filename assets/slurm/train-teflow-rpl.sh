ae481112e82dc49cd2ccd3547fd0bbcd366ecddc
Kin <kinzhangglimmer@gmail.com>
2026-03-31 14:46:58 +0200

    docs: update slurm file

--- assets/slurm/train-teflow-rpl.sh
+++ assets/slurm/train-teflow-rpl.sh
@@ -21,11 +21,11 @@ $PYTHON train.py slurm_id=$SLURM_JOB_ID wandb_mode=online wandb_project_name=tef
 # SOURCE="/local_storage/datasets/qingwen/data/h5py/waymo"
 # $PYTHON train.py slurm_id=$SLURM_JOB_ID wandb_mode=online wandb_project_name=teflow train_data=${SOURCE}/train val_data=${SOURCE}/valid \
 #      model=deltaflow save_top_model=2 val_every=3 train_aug=True voxel_size="[0.15, 0.15, 0.15]" point_cloud_range="[-38.4, -38.4, -3, 38.4, 38.4, 3]" num_workers=16 \
-#      epochs=15 optimizer.lr=1e-4 batch_size=2 num_frames=5 "+add_seloss={chamfer_dis: 1.0, static_flow_loss: 1.0, dynamic_chamfer_dis: 1.0, cluster_based_pc0pc1: 1.0}" \
+#      epochs=15 optimizer.lr=2e-3 batch_size=2 num_frames=5 "+add_seloss={chamfer_dis: 1.0, static_flow_loss: 1.0, dynamic_chamfer_dis: 1.0, cluster_based_pc0pc1: 1.0}" \
 #      +ssl_label=seflow_auto loss_fn=teflowLoss optimizer.name=Adam +optimizer.scheduler.name=StepLR +optimizer.scheduler.step_size=8 +optimizer.scheduler.gamma=0.5
 
 # SOURCE="/local_storage/datasets/qingwen/data/h5py/nus"
 # $PYTHON train.py slurm_id=$SLURM_JOB_ID wandb_mode=online wandb_project_name=teflow train_data=${SOURCE}/train val_data=${SOURCE}/val \
 #      model=deltaflow save_top_model=2 val_every=3 train_aug=True voxel_size="[0.15, 0.15, 0.15]" point_cloud_range="[-38.4, -38.4, -3, 38.4, 38.4, 3]" num_workers=16 \
-#      epochs=15 optimizer.lr=1e-4 batch_size=2 num_frames=5 "+add_seloss={chamfer_dis: 1.0, static_flow_loss: 1.0, dynamic_chamfer_dis: 1.0, cluster_based_pc0pc1: 1.0}" \
+#      epochs=15 optimizer.lr=2e-3 batch_size=2 num_frames=5 "+add_seloss={chamfer_dis: 1.0, static_flow_loss: 1.0, dynamic_chamfer_dis: 1.0, cluster_based_pc0pc1: 1.0}" \
 #      +ssl_label=seflow_auto loss_fn=teflowLoss optimizer.name=Adam +optimizer.scheduler.name=StepLR +optimizer.scheduler.step_size=8 +optimizer.scheduler.gamma=0.5
